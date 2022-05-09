d9476ba04c30297ee42d74bf779aeb58a9e5ed83
Kaitlyn Diaz <katediaz011@gmail.com>
2022-05-09 13:14:42 -0400

    added server.js

--- controllers/api/schools-routes.js
+++ controllers/api/schools-routes.js
@@ -1,4 +1,8 @@
+const axios = require('axios').default;
+
 let greatSchools;
+let greatSchoolsBaseURL = 'https://gs-api.greatschools.org/nearby-schools?'
+
 
 // This calls the API, just update the url to have your key's name.
 async function fetchKey() {
@@ -12,4 +16,9 @@ fetchKey().then((key) => {
   greatSchools = key.apiKey;
   console.log(greatSchools);
 });
+
+
+
+// 'https://gs-api.greatschools.org/nearby-schools?lat=37.7940627&lon=-122.2680029&limit=3&distance=5' \
+// --header 'x-api-key: myapikey’
 
