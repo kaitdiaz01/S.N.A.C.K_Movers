60b4537fff2c4a71739f756218798b9241778ab9
Kaitlyn Diaz <katediaz011@gmail.com>
2022-05-10 11:36:12 -0400

    added yelp/movers route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const censusRoutes = require('./census-routes');
 const economicRoutes = require('./economics-routes');
 const schoolRoutes = require('./school-routes');
-const yelpRoutes = require('./yelp-routes');
+const moverRoutes = require('./mover-routes');
 
 router.use('/census', censusRoutes);
 router.use('/economic', economicRoutes);
 router.use('/school', schoolRoutes);
-router.use('/yelp', yelpRoutes);
+router.use('/movers', moverRoutes);
 
 module.exports = router;
