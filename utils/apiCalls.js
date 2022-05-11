50f647496cef281154fe4ab667bba57ac055953a
nick quinn <nick.quinn@stadium1.com>
2022-05-11 13:39:10 -0400

    got the census api for median household income by zipcode working.  added it to apicalls.js

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 yelpBaseURL = "https://api.yelp.com/v3/businesses/search?";
 economicsBaseUrl = `https://apps.bea.gov/api/data/?UserID=${process.env.BEA_KEY}`;
 greatSchoolsBaseURL = "https://gs-api.greatschools.org/schools?limit=20&";
+censusBaseURL = `https://api.census.gov/data/2019/acs/acs5?get=NAME,B19301_001E&for=zip%20code%20tabulation%20area:`
+// 33477&in=state:12&key=${process.env.CENSUS_API}
 
 // class Yelp {
 //   constructor(name, rating, price)
@@ -31,4 +33,11 @@ module.exports = {
       return data.data;
     });
   },
+  
+  getCensus: (state, zipCode) => {
+    axios.get(censusBaseURL + zipCode + `&in=state:${state}&key=${process.env.CENSUS_API}`).then((data) =>{
+      console.log(data.data[1][1]);
+      return data.data[1][1];
+    })
+  }
 };
