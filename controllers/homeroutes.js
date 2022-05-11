86c53d3478a3e74e6674f80973bd5f7b1e6c8a93
Candace Robbins <canrob0522@gmail.com>
2022-05-11 10:11:46 -0500

    updated pages to homeroutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -9,17 +9,17 @@ router.get("/", (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
+router.get("/companies", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      include: [{ model: Project }],
+      include: [{ model: User }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render("profile", {
+    res.render("companies", {
       ...user,
       logged_in: true,
     });
@@ -31,7 +31,7 @@ router.get("/profile", withAuth, async (req, res) => {
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/");
     return;
   }
 
