10c35ccbbddd302638ba9970f217821f9c58f466
nick quinn <nick.quinn@stadium1.com>
2022-05-12 09:58:34 -0400

    added parameter to census-route

--- controllers/api/census-routes.js
+++ controllers/api/census-routes.js
@@ -2,15 +2,12 @@ const router = require("express").Router();
 const { Census } = require("../../models");
 const API = require("../../utils/apiCalls");
 
-router.get("/:zip", (req, res) => {
+router.get("/:state","/:zip", (req, res) => {
   API.getCensus(req.params.state, req.params.zip)
     .then((data) => {
         res.send(data.data[1][1]);
         console.log(data);
-      // data.data.businesses.forEach((element) => {
-      // console.log(
-      //   `${element.name} with a rating of ${element.rating} from ${element.review_count} reviews`
-      // );
+
     })
     .catch(function (error) {
       // handle error
