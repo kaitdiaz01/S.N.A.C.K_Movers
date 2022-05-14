f6320e049925cfacd046ef15b04fb883b29533db
Candace Robbins <canrob0522@gmail.com>
2022-05-14 10:08:14 -0500

    Added zip to end of school URL

--- public/js/categories.js
+++ public/js/categories.js
@@ -2,12 +2,28 @@ const locationEl = document.getElementById("location");
 const zipCode = document.getElementById("zipcode");
 const userinputZip = document.getElementById("grab-zip");
 
-
+window.onload = function() {
 locationEl.addEventListener("click", (event) => {
   event.preventDefault();
+  var currentZip = zipcode.value;
+  localStorage.setItem("storeZip", currentZip);
+  console.log(zipcode.value);
   console.log("clicked on button to go to comapnies");
   document.location.replace("/categories");
 });
+
+}
+
+// const locationEl = document.getElementById("location");
+// const zipCode = document.getElementById("zipcode");
+// const userinputZip = document.getElementById("grab-zip");
+
+
+// locationEl.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   console.log("clicked on button to go to comapnies");
+//   document.location.replace("/categories");
+// });
 
 
 
